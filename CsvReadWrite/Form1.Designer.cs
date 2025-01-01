adb4b52b0e3ba21ef558b712d3f71fe6afd77263
daichi <kabata0210@icloud.com>
2025-01-01 21:15:29 +0900

    画面設計完了

--- CsvReadWrite/Form1.Designer.cs
+++ CsvReadWrite/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            openFileDialogCsv = new OpenFileDialog();
+            saveFileDialogCsv = new SaveFileDialog();
+            dataGridViewCsv = new DataGridView();
+            textBoxInputCSVFileName = new TextBox();
+            textBoxOutputCSVFileName = new TextBox();
+            buttonCsvRead = new Button();
+            buttonCsvWrite = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialogCsv
+            // 
+            openFileDialogCsv.FileName = "*.csv";
+            openFileDialogCsv.InitialDirectory = ".\\";
+            // 
+            // saveFileDialogCsv
+            // 
+            saveFileDialogCsv.Filter = "CSVファイル|*.csv|すべてのファイル|*.*";
+            saveFileDialogCsv.InitialDirectory = ".\\";
+            // 
+            // dataGridViewCsv
+            // 
+            dataGridViewCsv.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCsv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCsv.Location = new Point(12, 41);
+            dataGridViewCsv.Name = "dataGridViewCsv";
+            dataGridViewCsv.RowTemplate.Height = 25;
+            dataGridViewCsv.Size = new Size(609, 279);
+            dataGridViewCsv.TabIndex = 0;
+            // 
+            // textBoxInputCSVFileName
+            // 
+            textBoxInputCSVFileName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxInputCSVFileName.Location = new Point(12, 12);
+            textBoxInputCSVFileName.Name = "textBoxInputCSVFileName";
+            textBoxInputCSVFileName.Size = new Size(609, 23);
+            textBoxInputCSVFileName.TabIndex = 1;
+            // 
+            // textBoxOutputCSVFileName
+            // 
+            textBoxOutputCSVFileName.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxOutputCSVFileName.Location = new Point(12, 326);
+            textBoxOutputCSVFileName.Name = "textBoxOutputCSVFileName";
+            textBoxOutputCSVFileName.Size = new Size(609, 23);
+            textBoxOutputCSVFileName.TabIndex = 2;
+            // 
+            // buttonCsvRead
+            // 
+            buttonCsvRead.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonCsvRead.Location = new Point(627, 11);
+            buttonCsvRead.Name = "buttonCsvRead";
+            buttonCsvRead.Size = new Size(75, 23);
+            buttonCsvRead.TabIndex = 3;
+            buttonCsvRead.Text = "CSV取得";
+            buttonCsvRead.UseVisualStyleBackColor = true;
+            // 
+            // buttonCsvWrite
+            // 
+            buttonCsvWrite.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCsvWrite.Location = new Point(627, 325);
+            buttonCsvWrite.Name = "buttonCsvWrite";
+            buttonCsvWrite.Size = new Size(75, 23);
+            buttonCsvWrite.TabIndex = 4;
+            buttonCsvWrite.Text = "CSV出力";
+            buttonCsvWrite.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(714, 361);
+            Controls.Add(buttonCsvWrite);
+            Controls.Add(buttonCsvRead);
+            Controls.Add(textBoxOutputCSVFileName);
+            Controls.Add(textBoxInputCSVFileName);
+            Controls.Add(dataGridViewCsv);
+            Name = "Form1";
+            Text = "CSVの読み書き";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialogCsv;
+        private SaveFileDialog saveFileDialogCsv;
+        private DataGridView dataGridViewCsv;
+        private TextBox textBoxInputCSVFileName;
+        private TextBox textBoxOutputCSVFileName;
+        private Button buttonCsvRead;
+        private Button buttonCsvWrite;
     }
 }
